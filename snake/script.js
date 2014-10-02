1e69f98956443b98048faa433992aa8bc41e7583
hjguyue <hjguyue@gmail.com>
2014-10-02 16:02:54 +0800

    add 7 titles, icon.png

--- snake/script.js
+++ snake/script.js
@@ -11,7 +11,7 @@
   var message = '<div style="padding-top: 105px">' +
     '您的长度是<span style="color: red"> %l </span>厘米<br>' +
     '击败了全国<span style="color: green"> %p% </span>的人<br>' +
-    '获得称号 %s</div>';
+    '获得称号 "%s"</div>';
 
   canvas.width = size;
   canvas.height = size;
@@ -92,8 +92,21 @@
       clearInterval(interval);
       var len = snake.size(), str = message;
       str = str.replace("%l", len);
-      str = str.replace("%p", Math.min(99, Math.floor(Math.log(len-4)/0.028)));
-      str = str.replace("%s", "bad ass");
+      str = str.replace("%p", Math.min(99, Math.floor(Math.log(Math.max(len, 5) - 4)/0.028)));
+      if (len <= 6)
+        str = str.replace("%s", "人穷智短");
+      else if (len <= 8)
+        str = str.replace("%s", "笨手笨脚");
+      else if (len <= 11)
+        str = str.replace("%s", "将勤补拙");
+      else if (len <= 15)
+        str = str.replace("%s", "冰雪聪明");
+      else if (len <= 19)
+        str = str.replace("%s", "眼捷手快");
+      else if (len <= 23)
+        str = str.replace("%s", "势如破竹");
+      else
+        str = str.replace("%s", "风驰电掣");
       web.apply("message", str);
       web.death();
     }
